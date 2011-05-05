9096f17768d4f9495a3badac9f1a2629bec73d05
marco.molinari1980@gmail.com <marco.molinari1980@gmail.com@8be8bb78-a580-ef64-62f8-33edb657bb8e>
2011-05-05 04:42:25 +0000

    aggiunta button bar

--- trunk/GestioneSpese/src/view/sidebar/ToggleBtn.java
+++ trunk/GestioneSpese/src/view/sidebar/ToggleBtn.java
@@ -6,7 +6,9 @@ import java.awt.Graphics;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 import javax.swing.JToggleButton;
+import javax.swing.SwingConstants;
 
 /**
  * La classe estende JToggleButton per modificarne il funzionamento di default
@@ -17,6 +19,7 @@ public class ToggleBtn extends JToggleButton {
 	ImageIcon                 i;
 
 	MyIcon                    icona;
+	private JPanel            padre;
 
 	public MyIcon getMyIcon() {
 		return icona != null ? icona : new MyIcon();
@@ -25,6 +28,11 @@ public class ToggleBtn extends JToggleButton {
 	public ToggleBtn(final String text) {
 		super(text);
 		s = text;
+	}
+
+	public ToggleBtn(final String text, final ImageIcon icon, JPanel padre) {
+		this(text, icon);
+		this.setPadre(padre);
 	}
 
 	public ToggleBtn(final String text, final ImageIcon icon) {
@@ -40,7 +48,13 @@ public class ToggleBtn extends JToggleButton {
 
 	@Override
 	public void paintComponent(final Graphics g) {
+		ImageIcon nuova = i;
+		ImageIcon sostituta = new ImageIcon();
+		this.setIcon(sostituta);
 		super.paintComponent(g);
+		this.setIcon(nuova);
+		i.paintIcon(this, g, 5, getHeight() / 2 - i.getIconHeight()
+		                / 2);
 		if (this.isSelected()) {
 			disegnaBottone(g, Color.GRAY, new Color(167, 243, 239));
 		}
@@ -61,6 +75,28 @@ public class ToggleBtn extends JToggleButton {
 			g.drawString(s, (i.getIconWidth()) + 4, (h + g.getFontMetrics()
 			                .getAscent()) / 2 - 1);
 		}
+	}
+
+	public void settaggioBottoneStandard() {
+		this.setRolloverEnabled(true);
+		this.setBorder(null);
+		this.setBackground(Color.WHITE);
+		this.setHorizontalAlignment(SwingConstants.LEFT); // allinea il
+		                                                  // contenuto del
+		                                                  // bottone a sinistra
+		if (this instanceof ToggleBtn) {
+			final Icon icona1 = (this).getMyIcon();
+			this.setRolloverIcon(icona1);
+		}
+		this.setRolloverEnabled(true);
+	}
+
+	public void setPadre(JPanel padre) {
+		this.padre = padre;
+	}
+
+	public JPanel getPadre() {
+		return padre;
 	}
 
 	/**
