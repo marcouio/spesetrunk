2ac4c50815898b8064a06d4345aa9bad0236cb21
marco.molinari1980 <marco.molinari1980@gmail.com>
2011-05-14 14:15:28 +0000

    modificato togglebtn

--- src/view/bottoni/ToggleBtn.java
+++ src/view/bottoni/ToggleBtn.java
@@ -10,39 +10,47 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.JToggleButton;
 import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
 
 /**
  * La classe estende JToggleButton per modificarne il funzionamento di default
  */
 public class ToggleBtn extends JToggleButton {
-	private static final long serialVersionUID           = 1L;
+	private static final long serialVersionUID    = 1L;
 	String                    s;
 	ImageIcon                 i;
 
 	MyIcon                    icona;
 	private JPanel            padre;
-	private final int         distanzaBordoImageX        = 10;
-	private final int         distanzaBordoXTestoDaIcona = 5;
+	private int               distanzaBordoImageX = 10;
+	private int               xPartenzaTesto      = 38;
+
+	Color                     colorForeground;
+	Color                     colorBackground;
 
 	public MyIcon getMyIcon() {
 		return icona != null ? icona : new MyIcon();
 	}
 
+	public ToggleBtn(final String text, final ImageIcon icon, final int xDistanzaBordoImmagine, int xPartenzaTesto) {
+		this(text, icon);
+		this.distanzaBordoImageX = xDistanzaBordoImmagine != -1 ? xDistanzaBordoImmagine : distanzaBordoImageX;
+		this.xPartenzaTesto = xPartenzaTesto != -1 ? xPartenzaTesto : this.xPartenzaTesto;
+	}
+
 	public ToggleBtn(final String text) {
-		super(text);
+		super("");
 		s = text;
 	}
 
 	public ToggleBtn(final String text, final ImageIcon icon, JPanel padre) {
 		this(text, icon);
 		this.setPadre(padre);
-		s = text;
-		i = icon;
-
 	}
 
 	public ToggleBtn(final String text, final ImageIcon icon) {
-		super(text, icon);
+		super("", icon);
 		s = text;
 		i = icon;
 	}
@@ -54,13 +62,23 @@ public class ToggleBtn extends JToggleButton {
 
 	@Override
 	public void paintComponent(final Graphics g) {
+		// setto l'icona a null per non farla disegnare dal super.paintcomponent
 		this.setIcon(new ImageIcon());
-		this.setText(s);
-		this.setIconTextGap(42);
+
+		// sono costretto a fare il super del paintComponent per via del
+		// rollover che altrimenti dovrei gestire io
 		super.paintComponent(g);
+
+		// invece di far disegnare il testo al paintcomponent ho settato il text
+		// a "" e il testo passato nel costruttore lo disegno qui così ho un
+		// maggiore controllo
+		g.drawString(s != null ? s : "", getLarghezzaImmagine(i) + distanzaBordoImageX + calcolaTextGap(i), (getHeight() + g.getFontMetrics().getAscent()) / 2 - 1);
+
+		// reimposto l'icona a quella passata
 		this.setIcon(i);
 		getIcon().paintIcon(this, g, distanzaBordoImageX, getHeight() / 2 - i.getIconHeight() / 2);
 		if (this.isSelected()) {
+			// effetto pulsante premuto ridefinito
 			disegnaBottone(g, Color.GRAY, new Color(167, 243, 239));
 		}
 	}// end paint
@@ -77,18 +95,40 @@ public class ToggleBtn extends JToggleButton {
 		if (i != null) {
 			g.drawImage(i.getImage(), distanzaBordoImageX, getHeight() / 2 - i.getIconHeight() / 2, null);
 		}
-		if (s != null && i != null) {
-			g.drawString(s, i.getIconWidth() + distanzaBordoImageX + distanzaBordoXTestoDaIcona, (h + g.getFontMetrics().getAscent()) / 2 - 1);
+		g.drawString(s != null ? s : "", getLarghezzaImmagine(i) + distanzaBordoImageX + calcolaTextGap(i), (h + g.getFontMetrics().getAscent()) / 2 - 1);
+	}
+
+	private int getLarghezzaImmagine(Icon i) {
+		if (i != null) {
+			return i.getIconWidth();
 		} else {
-			g.drawString(s, 25 + distanzaBordoImageX + distanzaBordoXTestoDaIcona, (h + g.getFontMetrics().getAscent()) / 2 - 1);
+			return 0;
+		}
+
+	}
+
+	private int calcolaTextGap(Icon i) {
+		if (i != null) {
+			if (xPartenzaTesto > (i.getIconWidth() - distanzaBordoImageX)) {
+				return xPartenzaTesto - (i.getIconWidth() - distanzaBordoImageX + 7);
+			} else {
+				return 0;
+			}
+		} else {
+			return xPartenzaTesto;
 		}
 	}
 
 	public void settaggioBottoneStandard() {
-		BorderFactory.createEtchedBorder();
+
+		Border bordo = BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.GRAY, Color.LIGHT_GRAY);
+		// Border bordo = BorderFactory.createEtchedBorder(EtchedBorder.RAISED,
+		// Color.RED, Color.GREEN);
+		this.setBorder(bordo);
 		this.setRolloverEnabled(true);
 		this.setBackground(Color.WHITE);
 		this.setHorizontalAlignment(SwingConstants.LEFT); // allinea il
+		                                                  // contenuto a sinitra
 		if (this instanceof ToggleBtn) {
 			final Icon icona1 = (this).getMyIcon();
 			this.setRolloverIcon(icona1);
