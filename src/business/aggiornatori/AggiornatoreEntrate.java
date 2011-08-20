0b66e0e2b8da211e39bd95426754eeaffedf0933
marco.molinari1980 <marco.molinari1980@gmail.com>
2011-08-20 17:13:52 +0000

    corretto baco nome aggiorna uscita

--- src/business/aggiornatori/AggiornatoreEntrate.java
+++ src/business/aggiornatori/AggiornatoreEntrate.java
@@ -1,11 +1,13 @@
 package business.aggiornatori;
 
+import domain.Entrate;
+
 public class AggiornatoreEntrate extends AggiornatoreBase implements IAggiornatore {
 
 	@Override
 	public boolean aggiorna() {
 		try {
-			return AggiornatoreManager.aggiornamentoGenerale(AggiornatoreManager.AGGIORNA_ENTRATE);
+			return AggiornatoreManager.aggiornamentoGenerale(Entrate.NOME_TABELLA);
 		} catch (final Exception e) {
 			e.printStackTrace();
 		}
