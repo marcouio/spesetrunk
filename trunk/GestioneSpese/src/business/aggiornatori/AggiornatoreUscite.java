2ce2a865c608462ff4c485e45efd9d7ab2db33bc
marco.molinari1980@gmail.com <marco.molinari1980@gmail.com@8be8bb78-a580-ef64-62f8-33edb657bb8e>
2011-08-20 17:13:52 +0000

    corretto baco nome aggiorna uscita

--- trunk/GestioneSpese/src/business/aggiornatori/AggiornatoreUscite.java
+++ trunk/GestioneSpese/src/business/aggiornatori/AggiornatoreUscite.java
@@ -1,11 +1,13 @@
 package business.aggiornatori;
 
+import domain.SingleSpesa;
+
 public class AggiornatoreUscite extends AggiornatoreBase implements IAggiornatore {
 
 	@Override
 	public boolean aggiorna() {
 		try {
-			return AggiornatoreManager.aggiornamentoGenerale(AggiornatoreManager.AGGIORNA_USCITE);
+			return AggiornatoreManager.aggiornamentoGenerale(SingleSpesa.NOME_TABELLA);
 		} catch (final Exception e) {
 			e.printStackTrace();
 		}
