c6285542bb9b3f9e19ea3fa4ab1e802fca82d9c9
marco.molinari1980@gmail.com <marco.molinari1980@gmail.com@8be8bb78-a580-ef64-62f8-33edb657bb8e>
2011-08-28 08:08:17 +0000

    inseriti messaggi

--- trunk/GestioneSpese/src/view/NewSql.java
+++ trunk/GestioneSpese/src/view/NewSql.java
@@ -15,7 +15,6 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.WindowConstants;
@@ -23,10 +22,11 @@ import javax.swing.WindowConstants;
 import view.font.ButtonF;
 import view.font.LabelTitolo;
 import view.font.TextAreaF;
-import business.Controllore;
+import business.AltreUtil;
 import business.DBUtil;
 import business.Database;
 import business.ascoltatori.AscoltatoreAggiornatoreNiente;
+import business.internazionalizzazione.I18NManager;
 import domain.CatSpese;
 import domain.Entrate;
 import domain.Gruppi;
@@ -67,7 +67,7 @@ public class NewSql extends OggettoVistaBase {
 			areaSql = new TextAreaF();
 			areaSql.setBounds(1, 1, 727, 71);
 			this.add(areaSql);
-			areaSql.setText("Inserisci qui il tuo codice sql");
+			areaSql.setText(I18NManager.getSingleton().getMessaggio("inssql"));
 			areaSql.setWrapStyleWord(true);
 			jsp = new JScrollPane(areaSql);
 			jsp.setBounds(134, 54, 715, 74);
@@ -76,12 +76,12 @@ public class NewSql extends OggettoVistaBase {
 			bottoneSvuota = new ButtonF();
 			bottoneSvuota.setBounds(37, 94, 75, 34);
 			this.add(bottoneSvuota);
-			bottoneSvuota.setText("Svuota");
+			bottoneSvuota.setText(I18NManager.getSingleton().getMessaggio("svuota"));
 
 			bottone = new ButtonF();
 			bottone.setBounds(37, 54, 75, 34);
 			this.add(bottone);
-			bottone.setText("Esegui");
+			bottone.setText(I18NManager.getSingleton().getMessaggio("esegui"));
 
 			result = new TextAreaF();
 			result.setBounds(1, 245, 847, 103);
@@ -93,11 +93,11 @@ public class NewSql extends OggettoVistaBase {
 			labelResult.setBounds(420, 135, 70, 21);
 			labelResult.setFont(new Font("Eras Light ITC", Font.PLAIN, 14));
 			this.add(labelResult);
-			labelResult.setText("Result");
+			labelResult.setText(I18NManager.getSingleton().getMessaggio("result"));
 
 			final ButtonF buttonF = new ButtonF();
 			buttonF.setBackground(Color.WHITE);
-			final ImageIcon image = new ImageIcon("imgUtil/info.gif");
+			final ImageIcon image = new ImageIcon(AltreUtil.IMGUTILPATH+"info.gif");
 			buttonF.setIcon(image);
 			buttonF.setBounds(870, 80, 56, 48);
 			add(buttonF);
@@ -107,14 +107,14 @@ public class NewSql extends OggettoVistaBase {
 				protected void actionPerformedOverride(ActionEvent e) {
 					super.actionPerformedOverride(e);
 					final StringBuffer sb = new StringBuffer();
-					sb.append("Tabelle principali: \n");
+					sb.append(I18NManager.getSingleton().getMessaggio("tables")+": \n");
 					sb.append(Entrate.NOME_TABELLA + ", " + SingleSpesa.NOME_TABELLA + ", ");
 					sb.append(CatSpese.NOME_TABELLA + ", " + Utenti.NOME_TABELLA + ", " + Gruppi.NOME_TABELLA);
 					sb.append(", " + Note.NOME_TABELLA);
 					sb.append("\n\n");
-					sb.append("La consolle supporta sia operazioni di select, sia operazioni di modifica");
+					sb.append(I18NManager.getSingleton().getMessaggio("infoconsolle"));
 					sb.append("\n\n");
-					sb.append("Per maggiori informazioni e istruzioni, consultare il manuale nella sezione di help");
+					sb.append(I18NManager.getSingleton().getMessaggio("otherinfosql"));
 					final JDialog d = new JDialog();
 					d.setLayout(new BorderLayout());
 					d.setSize(400, 180);
@@ -133,7 +133,7 @@ public class NewSql extends OggettoVistaBase {
 					d.add(lt, BorderLayout.NORTH);
 					final ButtonF b = new ButtonF();
 					d.add(b, BorderLayout.CENTER);
-					b.setText("Chiudi");
+					b.setText(I18NManager.getSingleton().getMessaggio("close"));
 					b.setBounds(0, 150, d.getWidth(), 30);
 					b.addActionListener(new ActionListener() {
 
@@ -191,8 +191,7 @@ public class NewSql extends OggettoVistaBase {
 						result.setText(totale + "\n");
 
 					} catch (final Exception e1) {
-						Controllore.getLog().severe(e1.getMessage());
-						JOptionPane.showMessageDialog(null, e1.getMessage(), "Non ci siamo!", JOptionPane.ERROR_MESSAGE, new ImageIcon("imgUtil/index.jpeg"));
+						Alert.operazioniSegnalazioneErroreGrave(e1.getMessage());
 					}
 				}
 			});
