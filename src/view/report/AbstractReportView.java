d00a08ad17d76358100257be0d513f4f3671e0eb
marco.molinari1980 <marco.molinari1980@gmail.com>
2011-09-01 19:05:59 +0000

    quasi terminato scrittorereporttext

--- src/view/report/AbstractReportView.java
+++ src/view/report/AbstractReportView.java
@@ -1,5 +1,7 @@
 package view.report;
 
+import java.util.HashMap;
+
 import javax.swing.JFrame;
 
 public abstract class AbstractReportView extends JFrame {
@@ -9,12 +11,167 @@ public abstract class AbstractReportView extends JFrame {
 
 	public AbstractReportView() {
 	}
-	public AbstractReportView(ReportData reportData) {
+
+	public AbstractReportView(final ReportData reportData) {
 		this.setReportData(reportData);
 	}
-	
-	
-	
+
+	public void setUsciteAnnuali(final boolean hasUsciteAnnuali) {
+		if (!hasUsciteAnnuali) {
+			reportData.setUsciteAnnuali(null);
+		} else {
+			reportData.setUsciteAnnuali(reportData.generaUsciteAnnuali());
+		}
+	}
+
+	public Double getUsciteAnnuali() {
+		return reportData.getUsciteAnnuali();
+	}
+
+	public void setEntrateAnnuali(final boolean hasEntrateAnnuali) {
+		if (!hasEntrateAnnuali) {
+			reportData.setEntrateAnnuali(null);
+		} else {
+			reportData.setEntrateAnnuali(reportData.generaEntrateAnnuali());
+		}
+	}
+
+	public Double getEntrateAnnuali() {
+		return reportData.getEntrateAnnuali();
+	}
+
+	public void setEntrateMensili(final boolean hasEntrateMensili) {
+		if (!hasEntrateMensili) {
+			reportData.setEntrateMese(null);
+		} else {
+			reportData.setEntrateMese(reportData.generaEntrateMese());
+		}
+	}
+
+	public HashMap<String, Double> getEntrateMensili() {
+		return reportData.getEntrateMese();
+	}
+
+	public void setUsciteMensili(final boolean hasUsciteMensili) {
+		if (!hasUsciteMensili) {
+			reportData.setUsciteMese(null);
+		} else {
+			reportData.setUsciteMese(reportData.generaUsciteMese());
+		}
+	}
+
+	public HashMap<String, Double> getUsciteMensili() {
+		return reportData.getUsciteMese();
+	}
+
+	public void setEntrateCatMensili(final boolean hasEntrateCatMensili) {
+		if (!hasEntrateCatMensili) {
+			reportData.setEntrateCatMensili(null);
+		} else {
+			reportData.setEntrateCatMensili(reportData.generaEntrateCatMensili());
+		}
+	}
+
+	public String[][] getEntrateCatMensili() {
+		return reportData.getEntrateCatMensili();
+	}
+
+	public void setUsciteCatMensili(final boolean hasUsciteCatMensili) {
+		if (!hasUsciteCatMensili) {
+			reportData.setUsciteCatMensili(null);
+		} else {
+			reportData.setUsciteCatMensili(reportData.generaUsciteCatMensili());
+		}
+	}
+
+	public String[][] getUsciteCatMensili() {
+		return reportData.getUsciteCatMensili();
+	}
+
+	public void setEntrateCatAnnuali(final boolean hasEntrateCatAnnuali) {
+		if (!hasEntrateCatAnnuali) {
+			reportData.setEntrateCatAnnuali(null);
+		} else {
+			reportData.setEntrateCatAnnuali(reportData.generaEntrateCatAnnuali());
+		}
+	}
+
+	public HashMap<String, Double> getEntrateCatAnnuali() {
+		return reportData.getEntrateCatAnnuali();
+	}
+
+	public void setUsciteCatAnnuali(final boolean hasUsciteCatAnnuali) {
+		if (!hasUsciteCatAnnuali) {
+			reportData.setUsciteCatAnnuali(null);
+		} else {
+			reportData.setUsciteCatAnnuali(reportData.generaUsciteCatAnnuali());
+		}
+	}
+
+	public HashMap<String, Double> getUsciteCatAnnuali() {
+		return reportData.getUsciteCatAnnuali();
+	}
+
+	public void setUsciteVariabili(final boolean hasUsciteVariabili) {
+		if (!hasUsciteVariabili) {
+			reportData.setUsciteVariabili(null);
+		} else {
+			reportData.setUsciteVariabili(reportData.generaUsciteVariabili());
+		}
+	}
+
+	public Double getUsciteVariabili() {
+		return reportData.getUsciteVariabili();
+	}
+
+	public void setUsciteFutili(final boolean hasUsciteFutili) {
+		if (!hasUsciteFutili) {
+			reportData.setUsciteFutili(null);
+		} else {
+			reportData.setUsciteFutili(reportData.generaUsciteFutili());
+		}
+	}
+
+	public Double getUsciteFutili() {
+		return reportData.getUsciteFutili();
+	}
+
+	public void setAvanzo(final boolean hasAvanzo) {
+		if (!hasAvanzo) {
+			reportData.setAvanzo(null);
+		} else {
+			reportData.generaAvanzo();
+		}
+	}
+
+	public Double getAvanzo() {
+		return reportData.getAvanzo();
+	}
+
+	public void setMediaEntrate(final boolean hasMediaEntrate) {
+		if (!hasMediaEntrate) {
+			reportData.setMediaEntrate(null);
+		} else {
+			reportData.setMediaEntrate(reportData.generaMediaEntrate());
+		}
+	}
+
+	public Double getMediaEntrate() {
+		return reportData.getMediaEntrate();
+	}
+
+	public void setMediaUscite(final boolean hasMediaUscite) {
+		if (!hasMediaUscite) {
+			reportData.setMediaUscite(null);
+		} else {
+			reportData.setMediaUscite(reportData.generaMediaUscite());
+		}
+	}
+
+	public Double getMediaUscite() {
+		return reportData.getMediaUscite();
+	}
+
 	public void setReportData(ReportData reportData) {
 		this.reportData = reportData;
 	}
--- src/view/report/ScrittoreReportTxt.java
+++ src/view/report/ScrittoreReportTxt.java
@@ -4,35 +4,167 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Vector;
+
+import domain.CatSpese;
 
 import business.AltreUtil;
 import business.DBUtil;
+import business.cache.CacheCategorie;
 
-public class ScrittoreReportTxt implements IScrittoreReport{
+public class ScrittoreReportTxt implements IScrittoreReport {
 
 	private FileOutputStream file = null;
 	private Date dataRegistrazione = new Date();
 	private Date dataAggiornamento;
-	
+	String trattini = "--------------------------------------------------------------------------";
+
 	public ScrittoreReportTxt() throws FileNotFoundException {
-		
+
 	}
-	
-	private PrintStream creaStream()
-			throws FileNotFoundException {
+
+	private PrintStream creaStream() throws FileNotFoundException {
 		AltreUtil.deleteFileDaDirectory("./", "Rep");
 		final String data = DBUtil.dataToString(new Date(), "dd_MM_yyyy_HH_mm_ss");
 		file = new FileOutputStream("Report" + data + ".txt");
 		return new PrintStream(file);
 	}
-	
+
 	@Override
 	public boolean generaReport(final ReportData reportData) throws Exception {
 		PrintStream stream = creaStream();
 		stream.println("Report Entrate/Uscite realizzato il : "
 				+ DBUtil.dataToString(dataRegistrazione, "dd/MM/yyyy HH:mm"));
 		
+		if(reportData.getAvanzo()!=null){
+			
+		}
+		if(reportData.getUsciteAnnuali()!=null){
+			
+		}
+		if(reportData.getEntrateAnnuali()!=null){
+			
+		}
+		if(reportData.getMediaEntrate()!=null){
+			
+		}
+		if(reportData.getMediaUscite()!=null){
+			
+		}
+		if(reportData.getUsciteVariabili()!=null){
+			
+		}
+		if(reportData.getUsciteFutili()!=null){
+			
+		}
+		if(reportData.getUsciteMese()!=null){
+			HashMap<String, Double> usciteMese = reportData.getUsciteMese();
+			for (int i = 1; i <= 12; i++) {
+				StringBuffer sb = new StringBuffer();
+				sb.append("Le uscite per il mese " + Mesi.getMeseStringa(i));
+				sb.append(" sono: " + usciteMese.get(Integer.toString(i)) + ". ");
+				stream.print(sb);
+				stream.println(" ");
+			}
+			stream.print(trattini);
+			stream.println(" ");
+		}
 		
+		if(reportData.getEntrateMese()!=null){
+			HashMap<String, Double> entrateMese = reportData.getEntrateMese();
+			for (int i = 1; i <= 12; i++) {
+				StringBuffer sb = new StringBuffer();
+				sb.append("Le entrate per il mese " + Mesi.getMeseStringa(i));
+				sb.append(" sono: " + entrateMese.get(Integer.toString(i)) + ". ");
+				stream.print(sb);
+				stream.println(" ");
+			}
+			stream.print(trattini);
+			stream.println(" ");
+		}
+		
+		if(reportData.getUsciteCatAnnuali()!=null){
+			
+			Vector<CatSpese> categorie = CacheCategorie.getSingleton().getVettoreCategorie();
+			HashMap<String, Double> usciteAnnoCat = reportData.getUsciteCatAnnuali();
+			for (int i = 0; i < categorie.size(); i++) {
+				final CatSpese categoria = categorie.get(i);
+				
+				StringBuffer sb = new StringBuffer();
+				sb.append("Le uscite annuali delle categoria '" + categoria.getnome());
+				sb.append("' sono: " + usciteAnnoCat.get(Integer.toString(categoria.getidCategoria()))+". ");
+				stream.print(sb);
+				stream.println(" ");
+			}
+			stream.print(trattini);
+			stream.println(" ");
+
+		}
+		if(reportData.getEntrateCatAnnuali()!=null){
+			String entrateAnnoCat = "";
+			String[] nomiColonne = new String[2];
+			nomiColonne[0] = "Fisse";
+			nomiColonne[1] = "Variabili";
+			HashMap<String, Double> entriesAnnoCat = reportData.getEntrateCatAnnuali();
+			for (int i = 0; i < nomiColonne.length; i++) {
+				StringBuffer sb = new StringBuffer();
+				sb.append("Le entrate annuali delle categoria '"+ nomiColonne[i]);
+				sb.append("' sono: " +entriesAnnoCat.get(nomiColonne[i])+".");
+				stream.print(entrateAnnoCat);
+				stream.println(" ");
+			}
+			stream.print(trattini);
+			stream.println(" ");
+
+		}
+		if(reportData.getUsciteCatMensili()!=null){
+			Vector<CatSpese> categorie = CacheCategorie.getSingleton().getVettoreCategorie();
+			final int numColonne = categorie.size();
+			final String[] nomiColonne = new String[numColonne];
+
+			for (int i = 0; i < categorie.size(); i++) {
+				nomiColonne[i] = categorie.get(i).getnome();
+			}
+
+			for (int i = 0; i < 12; i++) {
+				for (int x = 0; x < categorie.size(); x++) {
+					try {
+						String[][] matrice = reportData.getUsciteCatMensili();
+						final String stampa = "Le uscite per la categoria '" + nomiColonne[x] + "' ed il mese "
+								+ Mesi.getMeseStringa(i + 1) + " sono: " + matrice[i][x] + ".";
+						stream.print(stampa);
+						stream.println(" ");
+					} catch (final Exception e1) {
+						e1.printStackTrace();
+					}
+				}
+			}
+			stream.print(trattini);
+			stream.println(" ");
+		}
+		if(reportData.getEntrateCatMensili()!=null){
+			String[] nomiColonne = new String[2];
+			nomiColonne[0] = "Fisse";
+			nomiColonne[1] = "Variabili";
+			String[][] matrice = new String[12][2];
+			for (int i = 0; i < 12; i++) {
+				for (int x = 0; x < 2; x++) {
+					try {
+						matrice = reportData.getEntrateCatMensili();
+						final String stampa = "Le entrate per la categoria '" + nomiColonne[x]
+								+ "' ed il mese " + Mesi.getMeseStringa(i + 1) + " sono: " + matrice[i][x] + ".";
+						stream.print(stampa);
+						stream.println(" ");
+						stream.println(" ");
+					} catch (final Exception e2) {
+						e2.printStackTrace();
+					}
+				}
+			}
+			stream.print(trattini);
+			stream.println(" ");
+		}
 		
 		return false;
 	}
@@ -40,13 +172,16 @@ public class ScrittoreReportTxt implements IScrittoreReport{
 	public Date getDataRegistrazione() {
 		return dataRegistrazione;
 	}
+
 	public void setDataRegistrazione(Date dataRegistrazione) {
 		this.dataRegistrazione = dataRegistrazione;
 	}
+
 	public Date getDataAggiornamento() {
 		dataAggiornamento = new Date();
 		return dataAggiornamento;
 	}
+
 	public void setDataAggiornamento(Date dataAggiornamento) {
 		this.dataAggiornamento = dataAggiornamento;
 	}
