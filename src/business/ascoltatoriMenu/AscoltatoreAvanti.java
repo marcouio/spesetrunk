eabf862d44ca39942fe024f9cdbe6c539ebd08cd
marco.molinari1980 <marco.molinari1980@gmail.com>
2012-08-14 21:27:13 +0000

    aggiunte throws

--- src/business/ascoltatoriMenu/AscoltatoreAvanti.java
+++ src/business/ascoltatoriMenu/AscoltatoreAvanti.java
@@ -11,7 +11,7 @@ import business.comandi.RedoCommand;
 public class AscoltatoreAvanti extends AscoltatoreAggiornatoreTutto {
 
 	@Override
-	protected void actionPerformedOverride(final ActionEvent e) {
+	protected void actionPerformedOverride(final ActionEvent e) throws Exception {
 		final CommandManager managerComandi = Controllore.getSingleton().getCommandManager();
 		managerComandi.invocaComando(new RedoCommand());
 	}
