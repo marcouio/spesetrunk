0f793679ed00c2857849260a0089116706baa038
Marco Molinari <marco.molinari1980@gmail.com>
2017-03-04 17:46:27 +0100

    SONAR

--- src/main/java/com/molinari/gestionespese/business/comandi/RedoCommand.java
+++ src/main/java/com/molinari/gestionespese/business/comandi/RedoCommand.java
@@ -5,28 +5,8 @@ import command.AbstractCommand;
 public class RedoCommand extends AbstractCommand {
 
 	@Override
-	public boolean execute() {
-		return true;
-	}
-
-	@Override
-	public boolean unExecute() {
-		return true;
-	}
-
-	@Override
 	public String toString() {
 		return "Effettuato comando 'Avanti'";
-	}
-
-	@Override
-	public void scriviLogExecute(final boolean isComandoEseguito) {
-
-	}
-
-	@Override
-	public void scriviLogUnExecute(final boolean isComandoEseguito) {
-
 	}
 
 }
